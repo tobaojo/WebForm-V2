bb163a5c456c45dd18adaffb673ea6d515b8bbb1
Toba Ojo <toba@Tobas-MacBook-Air.local>
2020-07-29 19:57:54 +0100

    started created checks for form

---
+++ app.js
@@ -0,0 +1,41 @@
+const form = document.querySelector('.web-form');
+const firstname = document.querySelector('#firstname');
+const surname = document.querySelector('#surname');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const password2 = document.querySelector('#cpassword');
+
+
+form.addEventListener('submit', function(e){
+e.preventDefault();
+
+checkRequired([firstname, surname, email, password, password2]);
+
+
+function checkRequired(inputArr){
+inputArr.forEach(function(input){
+  if (input.value.trim() === '') {
+    showError(input,`${getFieldName(input)} is Required`)
+  }
+  else {
+    showSuccess(input);
+  }
+})
+}
+});
+
+function showError(input, msg){
+  const formControl = input.parentElement;
+  formControl.className = 'form-control fail'
+  const small = formControl.querySelector('small');
+  small.innerText = msg;
+}
+
+function showSuccess(input){
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+function getFieldName(input){
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
